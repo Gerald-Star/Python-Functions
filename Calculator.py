2df4020c69d699d35e2094de23bd2c5eb2ecd6f9
Gerald-Star <chimagerald3@gmail.com>
2024-02-26 22:12:06 +0100

    added the command lambda on the buttons and clear button expressions

--- Calculator.py
+++ Calculator.py
@@ -4,12 +4,25 @@ win.title('Calculator')
 win.geometry('515x365')
 win.resizable(0, 0)
 
+
+def btn_click(item):
+    global expression
+    # ! this expressions takes any number clicked
+    expression = expression + str(item)
+    # ! the input of this set expression gets into the StringVar
+    input_text.set(expression)
+
+
+expression = ""
+input_text = StringVar()  # StringVar function is use to hold data in python
+
+
 # input fiend frame
 input_frame = Frame(win, width=312, height=50)
 input_frame.pack(side=TOP)
 
 input_field = Entry(input_frame, font=(
-    'arial', 18, 'bold'), width=45, justify=RIGHT)
+    'arial', 18, 'bold'), width=45, justify=RIGHT, textvariable=input_text)
 input_field.grid(row=0, column=0)
 
 # increase the input height of input field
@@ -19,49 +32,72 @@ input_field.pack(ipady=10)
 btns_frame = Frame(win, width=310, height=270)
 btns_frame.pack()
 
-# states the button on the center of the app
-clear = Button(btns_frame, text='C', width=38, height=3).grid(
+# After adding the command functionalities, # ! create a function to clear the input field
+
+
+def bt_clear():
+    global expression
+    # passing an empty string clears any input with the clear button
+    input_text.set("")
+
+# Create a function for the Equal button # ! pass this function to the equal button
+
+
+def bt_equal():
+    global expression
+    result = str(eval(expression))
+    input_text.set(result)
+    expression = ""
+
+
+# states the button on the center of the app # ! command=lambda: btn_click()
+# ! pass the clear button function on clear
+clear = Button(btns_frame, text='C', width=38, height=3, command=lambda: bt_clear()).grid(
     row=0, column=0, columnspan=3, padx=1, pady=1)
 divide = Button(btns_frame, text='/', width=10,
-                height=3).grid(row=0, column=3, padx=1, pady=1)
+                height=3, command=lambda: btn_click('/')).grid(row=0, column=3, padx=1, pady=1)
 
-# Button Second Row
-seven = Button(btns_frame, text='7', width=10, height=3).grid(
+# Button Second Row # ! use the command=lambda: btn_click(7) to apply the functionalities of buttons
+# ! lambda is an anonymous function, it can take any number of arguments
+
+seven = Button(btns_frame, text='7', width=10, height=3, command=lambda: btn_click(7)).grid(
     row=1, column=0, padx=1, pady=1)
-eight = Button(btns_frame, text='8', width=10, height=3).grid(
+eight = Button(btns_frame, text='8', width=10, height=3, command=lambda: btn_click(8)).grid(
     row=1, column=1, padx=1, pady=1)
-nine = Button(btns_frame, text='9', width=10, height=3).grid(
+nine = Button(btns_frame, text='9', width=10, height=3, command=lambda: btn_click(9)).grid(
     row=1, column=2, padx=1, pady=1)
-multiply = Button(btns_frame, text='*', width=10, height=3).grid(
+multiply = Button(btns_frame, text='*', width=10, height=3, command=lambda: btn_click('*')).grid(
     row=1, column=3, padx=1, pady=1)
 
-# ? button third row
-four = Button(btns_frame, text='4', width=10, height=3).grid(
+#  button third row # ! add the command=lambda: btn_click()
+
+four = Button(btns_frame, text='4', width=10, height=3, command=lambda: btn_click(4)).grid(
     row=2, column=0, padx=1, pady=1)
-five = Button(btns_frame, text='5', width=10, height=3).grid(
+five = Button(btns_frame, text='5', width=10, height=3, command=lambda: btn_click(5)).grid(
     row=2, column=1, padx=1, pady=1)
-six = Button(btns_frame, text='6', width=10, height=3).grid(
+six = Button(btns_frame, text='6', width=10, height=3, command=lambda: btn_click(6)).grid(
     row=2, column=2, padx=1, pady=1)
-minus = Button(btns_frame, text='-', width=10, height=3).grid(
+minus = Button(btns_frame, text='-', width=10, height=3, command=lambda: btn_click('-')).grid(
     row=2, column=3, padx=1, pady=1)
 
-# Button fourth row
+# Button fourth row # ! add the command=lambda: btn_click()
 
-one = Button(btns_frame, text='1', width=10, height=3).grid(
+one = Button(btns_frame, text='1', width=10, height=3, command=lambda: btn_click(1)).grid(
     row=3, column=0, padx=1, pady=1)
-two = Button(btns_frame, text='2', width=10, height=3).grid(
+two = Button(btns_frame, text='2', width=10, height=3, command=lambda: btn_click(2)).grid(
     row=3, column=1, padx=1, pady=1)
-three = Button(btns_frame, text='3', width=10, height=3).grid(
+three = Button(btns_frame, text='3', width=10, height=3, command=lambda: btn_click(3)).grid(
     row=3, column=2, padx=1, pady=1)
-plus = Button(btns_frame, text='+', width=10, height=3).grid(
+plus = Button(btns_frame, text='+', width=10, height=3, command=lambda: btn_click('+')).grid(
     row=3, column=3, padx=1, pady=1)
 
-# ? Button Fifth row
-zero = Button(btns_frame, text='0', width=24, height=3).grid(
+#  Button Fifth row # ! command=lambda: btn_click()
+
+zero = Button(btns_frame, text='0', width=24, height=3, command=lambda: btn_click('0')).grid(
     row=4, column=0, columnspan=2, padx=1, pady=1)
-point = Button(btns_frame, text='.', width=10, height=3).grid(
+point = Button(btns_frame, text='.', width=10, height=3, command=lambda: btn_click('.')).grid(
     row=4, column=2, padx=1, pady=1)
-equal = Button(btns_frame, text='=', width=10, height=3).grid(
+equal = Button(btns_frame, text='=', width=10, height=3, command=lambda: bt_equal()).grid(
     row=4, column=3, padx=1, pady=1)
 
 
